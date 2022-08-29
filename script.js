57a04d0580ed8087f09133cf58cbf671f2adcd52
Panucio31 <m.januska1990@gmail.com>
2022-08-29 18:53:59 +0300

    feat: Computer random choice function added

---
+++ script.js
@@ -0,0 +1,16 @@
+let rock = 0;
+let paper = 1;
+let scissors = 2;
+
+
+
+function getComputersChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    return choice;
+}
+
+// console.log(getComputersChoice())
+
+function playGame(playerSelection, computerSelection) {
+    let 
+}
