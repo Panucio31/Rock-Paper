ea2e2982c8dc7cc3b27f927afc97d0de51ac722b
Panucio31 <m.januska1990@gmail.com>
2022-08-30 19:00:37 +0300

    feat: game function working

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const computerSelectionArray = ["Rock", "Paper", "Scissors"];
-let result = "";
+let playerResult = 0;
+let computerResult = 0;
 let outcome = "";
+// const rock = document.querySelector('#rock')
+// rock.addEventListener('click', () => {
+
+// })
+
 const getComputerChoice = () => {
   let choice =
     computerSelectionArray[
@@ -9,74 +15,48 @@ const getComputerChoice = () => {
   return choice.toLowerCase();
 };
 
-const playerResult = () => {
-  if (playerChose === computerSelectionArray[0].toLowerCase()) {
-    result = `rock`;
-  } else if (playerChose === computerSelectionArray[1].toLowerCase()) {
-    result = `paper`;
-  } else if (playerChose === computerSelectionArray[2].toLowerCase()) {
-    result = `scissors`;
-  } else {
-    result = `There is no move like that`;
-  }
-
-  return result;
-};
-
 const playRound = (playerSelection, computerSelection) => {
   playerSelection = prompt(`Chose Your weapon!!`).toLowerCase().valueOf();
   computerSelection = getComputerChoice().valueOf();
-  
 
   if (playerSelection === computerSelection) {
     outcome = `It's a Tie`;
   } else if (playerSelection === "rock" && computerSelection === "paper") {
     outcome = `You lose!! Paper covers Rock`;
+    computerResult++;
   } else if (playerSelection === "paper" && computerSelection === "scissors") {
     outcome = `You lose!! Scissors cut Paper`;
+    computerResult++;
   } else if (playerSelection === "scissors" && computerSelection === "rock") {
     outcome = `You lose!! Rock Crushes Scissors`;
+    computerResult++;
   } else if (playerSelection === "paper" && computerSelection === "rock") {
     outcome = `You win!! Paper covers Rock`;
+    playerResult++;
   } else if (playerSelection === "scissors" && computerSelection === "paper") {
     outcome = `You win!! Scissors cut Paper`;
+    playerResult++;
   } else if (playerSelection === "rock" && computerSelection === "scissors") {
     outcome = `You win!! Rock Crushes Scissors`;
+    playerResult++;
   }
-  
+
   return outcome;
 };
 
-//console.log(playRound())
+// console.log(playRound())
 
 const game = () => {
-    let playerResult = 0;
-    let computerResult = 0;
+  for (let i = 0; i < 5; i++) {
+    playRound();
+    console.log(playerResult);
+    console.log(computerResult);
+  }
+  if (computerResult > playerResult) {
+    return `Computer Wins`;
+  } else {
+    return `You Win`;
+  }
+};
 
-    for (let i = 0; i < 5; i++){
-        playRound();
-        if (playRound().valueOf() === `It's a Tie`){
-            console.log(`tie`)
-        } else if (
-            playRound().valueOf() === `You lose!! Paper covers Rock`||
-            playRound().valueOf() === `You lose!! Scissors cut Paper`||
-            playRound().valueOf() === `You lose!! Rock Crushes Scissors`) {
-            
-            computerResult++
-            // if(computerResult > playerResult) {
-            //     alert(`computer wins`)
-            // }
-        } else if (
-            playRound().valueOf() === `You win!! Paper covers Rock`||
-            playRound().valueOf() === `You win!! Scissors cut Paper`||
-            playRound().valueOf() === `You win!! Rock Crushes Scissors`) {
-            
-            playerResult++
-            // if (computerResult < playerResult) {
-            //     alert(`Player Wins`)
-            // }
-        }
-    }
-}
-
-game();
+console.log(game());
