6c0fed8f0ea22267eecbc3aef2a29c7d5e96d4c7
Panucio31 <m.januska1990@gmail.com>
2022-08-30 12:10:32 +0300

    feat: play round function finished

--- script.js
+++ script.js
@@ -1,16 +1,56 @@
-let rock = 0;
-let paper = 1;
-let scissors = 2;
+
+const computerSelectionArray = ['Rock', 'Paper', 'Scissors']
+let result = '';
+let playerChose = prompt(`Chose Your weapon!!`).toLowerCase();
 
 
 
-function getComputersChoice() {
-    let choice = Math.floor(Math.random() * 3);
-    return choice;
+const getComputerChoice = () => {
+let choice = computerSelectionArray[Math.floor(Math.random() * computerSelectionArray.length)];
+return choice.toLowerCase();
 }
 
-// console.log(getComputersChoice())
 
-function playGame(playerSelection, computerSelection) {
-    let 
+// const playerChose = () => { 
+//     return playerResult;
+// }
+// console.log(playerChose())
+
+//let palyerSelection = prompt(`Chose Your weapon!!`).toLowerCase();
+
+
+const playerResult = () => {
+    
+    if(playerChose === computerSelectionArray[0].toLowerCase()) {
+        result = `rock`;
+    } else if (playerChose === computerSelectionArray[1].toLowerCase()) {
+        result = `paper`;
+    } else if (playerChose === computerSelectionArray[2].toLowerCase()) {
+        result = `scissors`
+    } else {
+        result = `There is no move like that`
+    }
+
+    return result
 }
+
+const playRound = (playerSelection, computerSelection) => {
+    playerSelection = playerResult().valueOf();
+    computerSelection = getComputerChoice().valueOf();
+    console.log(computerSelection)
+    if (playerSelection === computerSelection) {
+        console.log(computerSelection)
+        return `tie`;
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        console.log(playerChose)
+        return `Paper covers Rock`;
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return `Scissors cut Paper`;
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return `Rock Crushes Scissors`
+    }
+}
+
+console.log(playRound())
+
+
