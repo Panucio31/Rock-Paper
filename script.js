03264c535ab336bc0089ff17437d3358bdadd370
Panucio31 <m.januska1990@gmail.com>
2022-09-19 21:32:20 +0300

    refactor: :art: made code esear to understand
    added some functions, so that each function has only one purpose.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
-const selectionBtn = document.querySelectorAll("[data-btn]");
+const selectionBtn = document.querySelectorAll(".btn");
 const finalCol = document.querySelector("[data-final-col]");
 const computerScoreSpan = document.querySelector("[data-computer-scr]");
 const yourScoreSpan = document.querySelector("[data-your-scr]");
 const result = document.querySelector(".winer");
 const restart = document.querySelector(".restart");
-const buttonsContainer = document.querySelector('.select-btn')
+const buttonsContainer = document.querySelector(".select-btn");
+
+const STATE = {
+  rounds: 3,
+  score: 0,
+}
 
 const SELECTIONS = [
   {
@@ -24,49 +29,54 @@ const SELECTIONS = [
   },
 ];
 
+const declareWinner = () => {
+  if (
+    parseInt(computerScoreSpan.innerHTML) === STATE.rounds &&
+    parseInt(yourScoreSpan.innerHTML) < parseInt(computerScoreSpan.innerHTML)
+  ) {
+    buttonsContainer.classList.add("d-none");
+    result.innerHTML = "Computer WINS!!";
+    restart.classList.remove('btn-none')
+    restart.addEventListener("click", () => {
+      location.reload();
+    });
+  } else if (
+    parseInt(yourScoreSpan.innerHTML) === STATE.rounds &&
+    parseInt(yourScoreSpan.innerHTML) > parseInt(computerScoreSpan.innerHTML)
+  ) {
+    buttonsContainer.classList.add("d-none");
+    result.innerHTML = "You WIN!!";
+    restart.classList.remove('btn-none')
+    restart.addEventListener("click", () => {
+      location.reload();
+    });
+  }
+};
+
 selectionBtn.forEach((selectionBtn) => {
-  selectionBtn.addEventListener("click", (e) => {
+  selectionBtn.addEventListener("click", () => {
     const playerSelection = selectionBtn.dataset.btn;
     const selection = SELECTIONS.find((btn) => btn.name === playerSelection);
-    makeSelection(selection);
+    const computerSelection = getComputerChoice();
+    whoGetsPoint(selection, computerSelection);
   });
 });
 
-function makeSelection(btn) {
-  const computerSelection = getComputerChoice();
+function whoGetsPoint(btn, computerSelection) {
   const youAreWinner = whoWins(btn, computerSelection);
   const computerWins = whoWins(computerSelection, btn);
-
+  
   addResult(computerSelection, computerWins);
   addResult(btn, youAreWinner);
 
   if (youAreWinner) incrementScore(yourScoreSpan);
   if (computerWins) incrementScore(computerScoreSpan);
 
-  if (
-    parseInt(computerScoreSpan.innerHTML) === 5 &&
-    parseInt(yourScoreSpan.innerHTML) < parseInt(computerScoreSpan.innerHTML)
-  ) {
-    buttonsContainer.classList.add('d-none');
-    result.innerHTML = "Computer WINS!!";
-    restart.addEventListener("click", () => {
-      location.reload();
-    });
-  } else if (
-    parseInt(yourScoreSpan.innerHTML) === 5 &&
-    parseInt(yourScoreSpan.innerHTML) > parseInt(computerScoreSpan.innerHTML)
-  ) {
-    buttonsContainer.classList.add('d-none');
-    result.innerHTML = "You WIN!!";
-    restart.addEventListener("click", () => {
-      location.reload();
-      
-    });
-  }
+  declareWinner();
 }
 
 function incrementScore(scoreSpan) {
-  scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1;
+  scoreSpan.innerText = STATE.score++;
 }
 
 function addResult(selection, winner) {
